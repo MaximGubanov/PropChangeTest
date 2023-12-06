5b072d6d393a2ee89c0125295d8947f47279638a
gubanovmv <m_gubanov@bk.ru>
2023-12-06 17:04:23 +0300

    TestBranch1

---
+++ PropertyChangeExperiment/Views/DateView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace PropertyChangeExperiment.Views;
+
+public partial class DateView : UserControl
+{
+    public DateView()
+    {
+        InitializeComponent();
+    }
+}
